ebfa4e7beb5416714b8999ba247e9903fe9fed70
Vrezh <miqaelyan.vrej1999@gmail.com>
2021-03-17 14:27:20 +0400

    test

---
+++ static/scripts/scripts.js
@@ -0,0 +1,15 @@
+function regForm(){
+    login=document.querySelector("#login");
+    signup=document.querySelector("#signup");
+    login.style.display="none";
+    signup.style.display="flex";
+
+}
+
+function logForm(){
+    login=document.querySelector("#login");
+    signup=document.querySelector("#signup");
+    login.style.display="flex";
+    signup.style.display="none";
+
+}
